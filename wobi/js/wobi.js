0514917b7a43808639966ccb93dcc76328ae0ca9
adrian.yao <5192640@163.com>
2015-02-11 15:18:38 +0800

    refine

---
+++ wobi/js/wobi.js
@@ -0,0 +1,15 @@
+function resizeContent() {
+
+}
+
+function getScreenHeight() {
+	return window.screen.height;
+}
+
+function pressCloseButton(dom) {
+	dom.style.backgroundImage="url('images/accountsdk_2_charge_ad_btn_close_press.png')";
+}
+
+function unPressCloseButton(dom) {
+	dom.style.backgroundImage="url('images/accountsdk_2_charge_ad_btn_close.png')";
+}
