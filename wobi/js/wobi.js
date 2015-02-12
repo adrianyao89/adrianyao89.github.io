122b8c9b51ef3c4994db997e5e4b8a32c3fc36c7
adrian.yao <5192640@163.com>
2015-02-12 09:20:49 +0800

    update

--- wobi/js/wobi.js
+++ wobi/js/wobi.js
@@ -1,15 +1,31 @@
-function resizeContent() {
+window.onload = function(){
+    var winHeight = window.screen.height;
+    var winWidth = window.screen.width;
+    var div = document.getElementById("container");
+    div.style.width=winWidth + "px";
+    div.style.height=winWidth*1.5 + "px";
+    
+    var fontSize = winWidth/320 * 16; 
+    div.style.fontSize=fontSize + "px";
 
-}
+    var isCheck = false;
+    var checkBox = document.getElementById("rechargeAgreementCheckbox");
+    checkBox.onclick = function() {
+        if (isCheck) {
+            checkBox.style.backgroundImage="url('images/accountsdk_2_charge_ad_cb_unset.png')";
+        } else {
+            checkBox.style.backgroundImage="url('images/accountsdk_2_charge_ad_cb_set.png')";
+        }
+        isCheck = !isCheck;
+        window.uniaccount.onShowNextTimeChanged(isCheck);
+    };
 
-function getScreenHeight() {
-	return window.screen.height;
-}
+    document.getElementById("rechargeButton").onclick = function() {
+        window.uniaccount.onRecharge();
+    };
 
-function pressCloseButton(dom) {
-	dom.style.backgroundImage="url('images/accountsdk_2_charge_ad_btn_close_press.png')";
-}
+    document.getElementById("closeButton").onclick = function() {
+        window.uniaccount.closeButton();
+    };
 
-function unPressCloseButton(dom) {
-	dom.style.backgroundImage="url('images/accountsdk_2_charge_ad_btn_close.png')";
 }
